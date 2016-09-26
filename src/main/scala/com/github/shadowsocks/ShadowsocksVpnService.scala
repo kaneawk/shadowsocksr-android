067dc3a8cb08b980273dadd17ed60b6c6601118e
Max Lv <max.c.lv@gmail.com>
2016-09-26 13:54:34 +0800

    Refine DNS resolution
    # Conflicts:
    #	src/main/jni/pdnsd
    #	src/main/scala/com/github/shadowsocks/ShadowsocksNatService.scala
    #	src/main/scala/com/github/shadowsocks/ShadowsocksVpnService.scala

--- src/main/scala/com/github/shadowsocks/ShadowsocksVpnService.scala
+++ src/main/scala/com/github/shadowsocks/ShadowsocksVpnService.scala
@@ -280,7 +280,6 @@ class ShadowsocksVpnService extends VpnService with BaseService {
     })
     val cmd = ArrayBuffer[String](getApplicationInfo.dataDir + "/ss-tunnel"
       , "-V"
-      , "-u"
       , "-t", "10"
       , "-b", "127.0.0.1"
       , "-L", profile.dns
